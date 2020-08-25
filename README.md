22f53748a5a2c2920b15806fbb62c8d700c212e7
Amir Daryabak <amir.daryabak@gmail.com>
2020-08-26 00:49:22 +0430

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# [Firebase Firestore](https://github.com/amirdaryabak/FirebaseFirestore.git)
+
+**NOTE:** This is a simple app that work with FirebaseFirestore
---
+++ app/src/main/java/com/amirdaryabak/firebasefirestore/Person.kt
@@ -0,0 +1,7 @@
+package com.amirdaryabak.firebasefirestore
+
+data class Person(
+    var firstName: String = "",
+    var lastName: String = "",
+    var age: Int = -1
+)
